5381268fe620a7a909202efb60fc4eab210b6693
Robert Burton <dalek42@bubble.org>
2017-04-12 23:48:18 -0400

    Added the current files

---
+++ assets/navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-default navbar-collapse">
+	<div class="container">
+		<div>
+			<a class="navbar-brand" href="index.html">Electchester VHF Club</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Introduction</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Membership</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Application</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Officers</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Links</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Announcements</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Members' Web Pages</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Webrings</a>
+			</li>
+		</ul>
+	</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html  lang="en">
+  <head>
+    <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://v4-alpha.getbootstrap.com/favicon.ico">
+    <title>EVHF New Site</title>
+		<link rel="stylesheet" src="//normalize-css.googlecode.com/svn/trunk/normalize.css" />
+		<!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="assets/main.css">
+  </head>
+	
+  <body cz-shortcut-listen="true">
+		<?php
+			include 'assets/navbar.php';
+		?>
+		
+    <div class="container">
+			<div class="jumbotron" id="thejumbo">
+				<div class="row-fluid">
+					<img src="electchesterlogo.png" class="img-responsive">
+					<img src="VHFclub.png" class="img-responsive">
+				</div>
+					
+
+			</div>
+      <h1>The Electchester VHF Club</h1>
+			<h2>Welcome to EVHF.org</h2>
+			<h3>The K2HAM Repeater</h3>
+    </div>
+    
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="app.js"></script>
+  </body>
+	
+</html>
